856f98956356dcd5b4d24e3236c9c6c62892661e
jotarosakamiya <sakajoy9@gmail.com>
2022-10-05 19:11:32 +0900

    add: Instruction, readfile logic

--- fibo_simu.cpp
+++ fibo_simu.cpp
@@ -1,5 +1,49 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
 
+using namespace std;
 
-int main() {
-    
+struct Instruction {
+    // flag: 0 -> instruction, 1 -> label
+    bool flag;    
+
+    string label;
+
+    string opecode;
+
+    string rd;
+    string rs1;
+    string rs2;
+
+    int imm;
+    int offset;
+
+    int execute() {
+        return 0;
+    }
+};
+
+vector<Instruction> instructions;
+
+int main(int argc, char const *argv[]) {
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "error: an error occurred opening file.\n");
+        return 1;
+    }
+
+    while(feof(fp) == 0) {
+        int c = fgetc(fp);
+        if (c == -1) {
+            break;
+        }
+        else {
+            putchar(c);
+        }
+
+    }    
+
+    return 0;
 }
