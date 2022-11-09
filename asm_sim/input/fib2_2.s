031b8abdb58c7f0b25baf9d17a4bebcff610f468
josaka <sakajoy9@gmail.com>
2022-11-09 23:59:27 +0900

    add: input dir

---
+++ asm_sim/input/fib2_2.s
@@ -0,0 +1,15 @@
+ble.22:
+	jalr	x0, x1, 0
+end:
+	sw		x10, 0(x0)
+min_caml_start:
+	addi	x2, x2, -112
+	lui		x4, 0
+	ori		x4, x0, 10
+	sw		x1, 4(x2)
+	addi	x2, x2, 8	
+	jal		x1, fib.9
+	addi	x2, x2, -8	
+	lw		x1, 4(x2)
+	addi	x10, x4, 0
+	addi	x2, x2, 112
