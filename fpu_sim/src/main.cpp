3fe5024d2f4089d629334ea66184c78129d2e8e0
sakajo6 <sakajoy9@gmail.com>
2023-01-28 20:44:16 +0900

    add: fadd, fsub, fmul error check

--- fpu_sim/src/main.cpp
+++ fpu_sim/src/main.cpp
@@ -1,11 +1,14 @@
 #include "./fpu_check.hpp"
 
 int main() {
+    int N;
+    std::cin >> N;
+
     FPU_check fpu_check;
 
-    fpu_check.fadd();
-    fpu_check.fsub();
-    fpu_check.fdiv();
+    fpu_check.fadd(N);
+    fpu_check.fsub(N);
+    fpu_check.fmul(N);
     fpu_check.finv();
-    fpu_check.fmul();
+    fpu_check.fsqrt();
 }
